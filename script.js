bbca19b08f4ad513f69d556d8ddf8ee7f6e7adc5
Yoselyn Peralta <yoselynperalta@Yoselyns-Air.lan>
2024-04-30 22:56:35 -0400

    average salary and random employee is working, as well as being able to add multiple employees

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
-
+let employeesArray=[]
+let addMore= true
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
+  while(addMore){
+
+    const firstName = prompt('what is the first name?')
+    const lastName = prompt('what is hte last name?')
+    const salary = prompt('what is the salary?')
+    const newEmployee = {firstName, lastName, salary}
+    employeesArray.push(newEmployee)
+    addMore = confirm('do you want to add another employee?')
+  }
+  return employeesArray
+  //displayEmployees(employeesArray)
 }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+  const numberOfEmployees = employeesArray.length
+  let sum=0
+  for(let i=0; i<numberOfEmployees; i++){
+    sum+= parseInt(employeesArray[i].salary)
+  }
+  const averageSalary = sum/ numberOfEmployees
+  console.log('Average Salaray: ', averageSalary)
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  const randomIndex = parseInt(Math.random()* employeesArray.length)
+  const randomEmployee = employeesArray[randomIndex]
+  console.log("Random Employee: " + randomEmployee.firstName + ',' + randomEmployee.lastName)
 }
 
 /*
